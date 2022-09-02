2a4459b8eb485b59995d59c241b2ac96b09540ce
demog <dg08gg@mail.ru>
2022-09-02 13:31:00 +0300

    Главный файл в приложении. Очистил от ненужного, позже сделал из него BrowserRouter. Перемещает через страницы

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./utils/Home";
+import Todo from "./utils/Todo";
+import Films from "./utils/Films";
+import NavBar from "./components/UI/Navbar/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/todo' element={<Todo />} />
+        <Route path='/films' element={<Films />} />
+        <Route path='*' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
