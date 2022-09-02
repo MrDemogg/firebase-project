436006ab46c5b0a9f4f832fc3da45bdc6226f42b
demog <dg08gg@mail.ru>
2022-09-02 13:34:36 +0300

    Встречающая страница. Привязана к App.js

---
+++ src/utils/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Card} from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <Card>
+      <Card.Body>
+        <center><h1>Home_work_63</h1></center>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Home;
